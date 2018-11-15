19b40cdcf41243ca170f09d2e627a1958ea71f85
dsubject <daniellesubject@gmail.com>
2018-11-15 08:30:14 -0500

    db connected

--- index.js
+++ index.js
@@ -1,8 +1,18 @@
 const hapi = require('hapi');
+const mongoose = require('mongoose');
+var dotenv = require('dotenv');
+
+dotenv.config();
 
 const server = hapi.server({
   port: 4000,
   host: 'localhost'
+});
+
+mongoose.connect(process.env.MONGOLAB_URI);
+
+mongoose.connection.once('open', () => {
+  console.log('connected to database');
 });
 
 const init = async () => {
